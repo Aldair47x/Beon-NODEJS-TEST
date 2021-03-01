dd060d01c570ec22313d9201f46674d04815e3d6
Aldair47x <aldair47x@gmail.com>
2021-02-28 21:00:55 -0500

    Finishing with all tests done

---
+++ index.js
@@ -0,0 +1,46 @@
+
+const http = require("http");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const indexRouter = require('./routes/index');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+const host = 'localhost';
+const port = 8000;
+
+
+
+app.use(bodyParser.json({
+  type: function() {
+      return true;
+  }
+}));
+
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+
+app.use(bodyParser.json({
+  type: function() {
+      return true;
+  }
+}));
+
+app.use('/', indexRouter);
+
+
+
+const server = http.createServer(app);
+
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
+
+module.exports = server;
+module.exports = app;
